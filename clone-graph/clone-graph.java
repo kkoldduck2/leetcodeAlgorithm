ef0fd5d6265d00bec135ae5b18b338b7b6cb3058
kkoldduck2 <vkqmffjtm@naver.com>
2021-09-30 11:24:11 +0900

    Time: 26 ms (54.61%), Space: 39.2 MB (69.94%) - LeetHub

---
+++ clone-graph/clone-graph.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Integer, Node> hm = new HashMap<>();
+    public Node clonelogic(Node node){
+        if (node == null) return null;
+        if(hm.containsKey(node.val)){
+            return hm.get(node.val);
+        }
+        
+        Node myNode = new Node(node.val);
+        
+        hm.put(myNode.val, myNode);
+        for(Node nb : node.neighbors){
+            myNode.neighbors.add(clonelogic(nb));
+        }
+        
+        return myNode;
+    }
+    public Node cloneGraph(Node node) {
+        return clonelogic(node);
+    }
+}
