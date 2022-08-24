6012001e4699d44f446e1ba4c0fbb48f4c1f6028
kkoldduck2 <vkqmffjtm@naver.com>
2022-08-24 09:56:09 +0900

    Time: 109 ms (54.80%), Space: 15.2 MB (24.20%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map={}
-        # 키와 값을 바꿔서 딕셔너리로 저장  (값이 같은게 있으면 맨 마지막 거만 저장되잖아..)
-        for i, num in enumerate(nums):
-            nums_map[num] = i
-
-        # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
-        for i, num in enumerate(nums):
-            if target - num in nums_map and i!=nums_map[target - num]:
-                return [i, nums_map[target - num]]
+        hm = {}
+        for i,num in enumerate(nums):
+            if target - num in hm:
+                return [hm[target - num], i]
+            else:
+                hm[num] = i
