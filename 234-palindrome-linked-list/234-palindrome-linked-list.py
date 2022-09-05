9ac0602cfecf7032dfc797a4110f5c410fa2bf74
kkoldduck2 <vkqmffjtm@naver.com>
2022-09-05 11:46:05 +0900

    Time: 1688 ms (8.53%), Space: 31.1 MB (98.37%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -6,24 +6,28 @@
 class Solution:
     # 연결 리스트 입력값을 파이썬의 리스트로 변환해 푼다
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q = list()
+        rev = None  #느린 런너가 연결리스트를 역순으로 생성
+        slow = fast = head
         
-        if not head:
-            return True
+        #런너를 이용해 역순 연결 리스트 구성
+        while fast and fast.next:   # next가 존재하지 않을 때까지
+            fast = fast.next.next
+            # 다중 할당 : 같은 시점에 한꺼번에 변수 할당. 
+            rev, rev.next, slow = slow, rev, slow.next
+            # 아래와 같이 3줄로 할당하면 문제가 안풀린다.
+            # rev = slow
+            # rev.next = rev
+            # slow = slow.next
         
-        node = head
+        # 홀수, 짝수일 때 처리 다름 -> 홀수면 slow 한칸 더 이동 (가운데 값 처리 안함)
+        if fast:
+            slow = slow.next
         
-        # 리스트 변환
-        while node is not None:
-            q.append(node.val)
-            node = node.next
+        # 팰린드롬 여부 확인
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
         
-        # 팰린드롬 판별
-        while len(q) > 1:
-            if q.pop(0) != q.pop():
-                return False
-        
-        return True
+        return rev == None  # rev == None이니
         
         
         
